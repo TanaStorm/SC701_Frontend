e70f27628fe695c41a26876c21f58bdba872b16d
TanaStorm <karendn20@gmail.com>
2022-04-14 19:10:18 -0600

    Backend connection

--- FE/Controllers/CategoriesController.cs
+++ FE/Controllers/CategoriesController.cs
@@ -5,12 +5,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using FE.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace FE.Controllers
 {
     public class CategoriesController : Controller
     {
-        string baseurl = "http://localhost:59634/"; //NEW
+        string baseurl = "http://localhost:62460/"; //NEW //OJO es el mismo puerto del backend BE.API
         //private readonly NorthwindContext _context;
 
         //public CategoriesController(NorthwindContext context)
@@ -22,27 +24,39 @@ namespace FE.Controllers
         public async Task<IActionResult> Index()
         {
             List<Categories> list = new List<Categories>();//NEW
-            //return View(await _context.Categories.ToListAsync());
-            return View(); //NEW
+
+            using (var cl = new HttpClient())
+            {
+                cl.BaseAddress = new Uri(baseurl);//El url donde invocamos el backend
+                cl.DefaultRequestHeaders.Clear();
+                cl.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));//Declaramos que queremos info convertida en JSON
+                HttpResponseMessage res = await cl.GetAsync("api/Categories");//Get async del API Categories
+
+                if (res.IsSuccessStatusCode)
+                {
+                    var auxres = res.Content.ReadAsStringAsync().Result;
+                    list = JsonConvert.DeserializeObject<List<Categories>>(auxres);
+                }
+            }
+                //return View(await _context.Categories.ToListAsync());
+                return View(list); //NEW
         }
 
         // GET: Categories/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            //if (id == null)
-            //{
-            //    return NotFound();
-            //}
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-            //var categories = await _context.Categories
-            //    .FirstOrDefaultAsync(m => m.CategoryId == id);
-            //if (categories == null)
-            //{
-            //    return NotFound();
-            //}
+            var categories = GetById(id);
+            if (categories == null)
+            {
+                return NotFound();
+            }
 
-            //return View(categories);
-            return View(); //NEW
+            return View(categories);
         }
 
         // GET: Categories/Create
@@ -58,31 +72,44 @@ namespace FE.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("CategoryId,CategoryName,Description,Picture")] Categories categories)
         {
-            //if (ModelState.IsValid)
-            //{
-            //    _context.Add(categories);
-            //    await _context.SaveChangesAsync();
-            //    return RedirectToAction(nameof(Index));
-            //}
-            //return View(categories);
-            return View(); //NEW
+            if (ModelState.IsValid)
+            {
+                //    _context.Add(categories);
+                //    await _context.SaveChangesAsync();
+                //    return RedirectToAction(nameof(Index));
+                using (var cl = new HttpClient())
+                {
+                    cl.BaseAddress = new Uri(baseurl);//El url donde invocamos el backend
+                    var content = JsonConvert.SerializeObject(categories);
+                    var buffer = System.Text.Encoding.UTF8.GetBytes(content);
+                    var byteContent = new ByteArrayContent(buffer);
+                    byteContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json");//Declaramos que queremos info convertida en JSON
+                    var postTask = cl.PostAsync("api/Categories", byteContent).Result;// POST del API Categories
+
+                    if (postTask.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+            }
+            return View(categories);
         }
 
         // GET: Categories/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            //if (id == null)
-            //{
-            //    return NotFound();
-            //}
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-            //var categories = await _context.Categories.FindAsync(id);
-            //if (categories == null)
-            //{
-            //    return NotFound();
-            //}
-            //return View(categories);
-            return View(); //NEW
+            var categories = GetById(id);
+            if (categories == null)
+            {
+                return NotFound();
+            }
+            return View(categories);
+
         }
 
         // POST: Categories/Edit/5
@@ -92,52 +119,63 @@ namespace FE.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("CategoryId,CategoryName,Description,Picture")] Categories categories)
         {
-            //if (id != categories.CategoryId)
-            //{
-            //    return NotFound();
-            //}
+            if (id != categories.CategoryId)
+            {
+                return NotFound();
+            }
 
-            //if (ModelState.IsValid)
-            //{
-            //    try
-            //    {
-            //        _context.Update(categories);
-            //        await _context.SaveChangesAsync();
-            //    }
-            //    catch (DbUpdateConcurrencyException)
-            //    {
-            //        if (!CategoriesExists(categories.CategoryId))
-            //        {
-            //            return NotFound();
-            //        }
-            //        else
-            //        {
-            //            throw;
-            //        }
-            //    }
-            //    return RedirectToAction(nameof(Index));
-            //}
-            //return View(categories);
-            return View(); //NEW
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    using (var cl = new HttpClient())
+                    {
+                        cl.BaseAddress = new Uri(baseurl);//El url donde invocamos el backend
+                        var content = JsonConvert.SerializeObject(categories);
+                        var buffer = System.Text.Encoding.UTF8.GetBytes(content);
+                        var byteContent = new ByteArrayContent(buffer);
+                        byteContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json");//Declaramos que queremos info convertida en JSON
+                        var postTask = cl.PutAsync("api/Categories/" + id, byteContent).Result;// PUT del API Categories
+
+                        if (postTask.IsSuccessStatusCode)
+                        {
+                            return RedirectToAction("Index");
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    if (!CategoriesExists(categories.CategoryId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categories);
+
         }
 
         // GET: Categories/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            //if (id == null)
-            //{
-            //    return NotFound();
-            //}
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-            //var categories = await _context.Categories
-            //    .FirstOrDefaultAsync(m => m.CategoryId == id);
-            //if (categories == null)
-            //{
-            //    return NotFound();
-            //}
+            var categories = GetById(id);
+            if (categories == null)
+            {
+                return NotFound();
+            }
 
-            //return View(categories);
-            return View(); //NEW
+            return View(categories);
+
         }
 
         // POST: Categories/Delete/5
@@ -145,15 +183,45 @@ namespace FE.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            //var categories = await _context.Categories.FindAsync(id);
-            //_context.Categories.Remove(categories);
-            //await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            var categories = GetById(id);
+            using (var cl = new HttpClient())
+            {
+                cl.BaseAddress = new Uri(baseurl);//El url donde invocamos el backend
+                cl.DefaultRequestHeaders.Clear();
+                cl.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));//Declaramos que queremos info convertida en JSON
+                HttpResponseMessage res = await cl.DeleteAsync("api/Categories/" + id);//Delete async del API Categories
+
+                if (res.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return RedirectToAction("Index");
         }
 
-        //private bool CategoriesExists(int id)
-        //{
-        //    return _context.Categories.Any(e => e.CategoryId == id);
-        //}
+        private bool CategoriesExists(int id)//deme cualquier objeto con este id, y validar si existe
+        {
+            return GetById(id)!= null;
+        }
+
+
+        private Categories GetById(int? id)//El simbolo de pregunta es xq permite nulos.
+    {
+        Categories aux = new Categories();
+            using (var cl = new HttpClient())
+            {
+                cl.BaseAddress = new Uri(baseurl);//El url donde invocamos el backend
+                cl.DefaultRequestHeaders.Clear();
+                cl.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));//Declaramos que queremos info convertida en JSON
+                HttpResponseMessage res = cl.GetAsync("api/Categories/" + id).Result;//Get async del API Categories
+
+                if (res.IsSuccessStatusCode)
+                {
+                    var auxres = res.Content.ReadAsStringAsync().Result;
+                    aux = JsonConvert.DeserializeObject<Categories>(auxres);
+                }
+            }
+            return aux;
+        }
     }
 }
